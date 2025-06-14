6b72f0ee3ceefb3d0636e9382cfce6c7ca8ef217
Nick Gonner <nggonner@gmail.com>
2025-06-14 13:46:00 -0500

    Add AudioListener to player and basic tank controls

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -2,3 +2,21 @@ class_name Player extends CharacterBody2D
 
 
 const SPEED = 300.0
+const ROTATION_SPEED = PI / 2
+
+func _init():
+	$AudioListener2D.make_current()
+
+func _process(delta):
+	var rotation_direction = 0
+	if Input.is_action_pressed("ui_left"):
+		rotation_direction += -1
+	if Input.is_action_pressed("ui_right"):
+		rotation_direction += 1
+	rotation += rotation_direction * ROTATION_SPEED * delta
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN.rotated(-rotation) * SPEED
+	position += velocity * delta
