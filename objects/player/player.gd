cce7bf67eea2340d9623099df87ebd2a2964c30b
mightysorensen <mightysorensen@hotmail.com>
2025-06-15 08:01:53 -0500

    feat: add debug draw of player direction

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -3,11 +3,23 @@ extends CharacterBody2D
 
 @onready var state_machine = $StateMachine
 @onready var animations: AnimatedSprite2D = $Animations
-
 @onready var move_component = $MoveComponent
-const SPEED = 300.0
+
+@export var debug : bool = true
+
+var default_font: Font
+var default_font_size: int
+
+func _draw() -> void:
+	if debug:
+		var end_point = Vector2.ZERO + Vector2.from_angle(Globals.player_direction) * 100
+		draw_line(Vector2.ZERO, end_point, Color.GREEN)
+		draw_string(default_font, end_point, str(round(rad_to_deg(Globals.player_direction))),0, -1, 12, Color.GREEN)
 
 func _ready() -> void:
+	default_font = ThemeDB.fallback_font
+	default_font_size = ThemeDB.fallback_font_size
+	
 	#initialize state machine
 	state_machine.init(self, animations, move_component)
 	$AudioListener2D.make_current()
@@ -19,4 +31,6 @@ func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
 
 func _process(delta: float) -> void:
+	if debug:
+		queue_redraw()
 	state_machine.process_frame(delta)
