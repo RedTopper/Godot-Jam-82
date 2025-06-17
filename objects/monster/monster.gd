884d42661cab61570ed66acac25ee7d8ec5fc85d
Nick Gonner <nggonner@gmail.com>
2025-06-16 19:24:27 -0500

    feat(guard): Setup guard with vision collision boxes

--- objects/monster/monster.gd
+++ objects/monster/monster.gd
@@ -7,13 +7,7 @@ const SPEED = 300.0
 
 func _ready() -> void:
 	#initialize state machine
-	state_machine.init(self)
-
-func _unhandled_input(event: InputEvent) -> void:
-	state_machine.process_input(event)
-
-func _physics_process(delta: float) -> void:
-	state_machine.process_physics(delta)
-
+	pass
+	
 func _process(delta: float) -> void:
-	state_machine.process_frame(delta)
+	pass
