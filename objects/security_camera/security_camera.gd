cf8709fdd4398b33bcce139d802c28f94b29f2f0
mightysorensen <mightysorensen@hotmail.com>
2025-06-20 17:21:13 -0500

    feat: add security cameras

---
+++ objects/security_camera/security_camera.gd
@@ -0,0 +1,31 @@
+class_name SecurityCamera
+extends CharacterBody2D
+
+@onready var _state_machine = $StateMachine
+
+@export var debug : bool = false
+@export var min_angle_deg : float = 0.0
+@export var max_angle_deg : float = 90.0
+@export var scan_speed: float = 9.0 # deg/s
+@export var alarm_reset_time: float = 10.0 # s
+
+var _debug_font: Font
+var _debug_font_size: int
+
+func _ready() -> void:
+	_debug_font = ThemeDB.fallback_font
+	_debug_font_size = ThemeDB.fallback_font_size
+	
+	#_state_machine = $StateMachine
+	_state_machine.init(self, $Images, null, null)
+
+func _physics_process(delta: float) -> void:
+	_state_machine.process_physics(delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+	_state_machine.process_input(event)
+
+func _process(delta: float) -> void:
+	if debug:
+		queue_redraw()
+	_state_machine.process_frame(delta)
---
+++ objects/security_camera/states/scanning.gd
@@ -0,0 +1,35 @@
+extends State
+
+@export var alarm_state: State
+
+var _camera : SecurityCamera
+var _scan_direction : float = 1.0
+
+var _spider_detected: bool = false
+
+func enter() -> void:
+	_camera = parent
+	
+	%VisionLight.color = Color.AQUA
+	
+func exit() -> void:
+	pass
+
+func process_physics(delta: float) -> State:
+	_camera.rotation_degrees += _camera.scan_speed * _scan_direction * delta
+	
+	if _camera.rotation_degrees > _camera.max_angle_deg:
+		_camera.rotation_degrees = _camera.max_angle_deg
+		_scan_direction *= -1.0
+	elif _camera.rotation_degrees < _camera.min_angle_deg:
+		_camera.rotation_degrees = _camera.min_angle_deg
+		_scan_direction *= -1.0
+	
+	if _spider_detected:
+		return alarm_state
+	
+	return null
+
+func _on_vision_box_body_entered(body: Node2D) -> void:
+	if body.name == "Spider":
+		_spider_detected = true
