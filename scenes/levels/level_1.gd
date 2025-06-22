726bcb5a8f5be139183c566f03fac26f305cbdbe
Andrew Peters <adpeters5@gmail.com>
2025-06-22 13:43:38 -0500

    feat: win button

--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -2,3 +2,7 @@ extends Node
 
 func _ready() -> void:
 	$DialogueAnimations.play("intro")
+
+func _on_win_button_pressed():
+	print("Button Pressed!")
+	get_tree().change_scene("res://scenes/menu/lose_menu/lose_menu.tscn")
